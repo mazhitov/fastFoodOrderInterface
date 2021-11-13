5c8150e6763578cfb166cb0de36b59a0afd24514
Mazhitov Keneshbek <majitovkesha@gmail.com>
2021-11-13 17:01:12 +0600

    add Get Total Price of order items

--- src/app/order-details/order-details.component.ts
+++ src/app/order-details/order-details.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Food} from "../../shared/food.model";
 
 @Component({
   selector: 'app-order-details',
   templateUrl: './order-details.component.html',
   styleUrls: ['./order-details.component.css']
 })
-export class OrderDetailsComponent implements OnInit {
+export class OrderDetailsComponent{
+  @Input() foodItems: Food[] = [];
+  @Input() drinkItems: Food[] = [];
+  @Output() changeFoodsItem = new EventEmitter<Food[]>();
+  @Output() changeDrinksItem = new EventEmitter<Food[]>();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onChangeFoodsItem($event: Food[]) {
+    this.foodItems = $event;
+    this.changeFoodsItem.emit(this.foodItems);
   }
 
+  onChangeDrinksItem($event: Food[]) {
+    this.drinkItems = $event;
+    this.changeDrinksItem.emit(this.drinkItems);
+  }
+
+  getTotalSum() {
+    const foodTotal = this.foodItems.reduce((acc, item)=> {
+      return acc + item.getTotal();
+    },0);
+    return foodTotal + this.drinkItems.reduce((acc, item) => {
+      return acc + item.getTotal();
+    },0);
+  }
 }
