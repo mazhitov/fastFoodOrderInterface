b8692cacc0604d736e7c0ceec023670cbfa9623a
Mazhitov Keneshbek <majitovkesha@gmail.com>
2021-11-13 11:36:18 +0600

    create component app-addItems

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AddItemsComponent } from './add-items/add-items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddItemsComponent
   ],
   imports: [
     BrowserModule
